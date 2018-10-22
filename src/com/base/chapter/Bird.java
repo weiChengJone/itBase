5550e3d0d00bacb3c5bac11a6e1145179b9af77c
yangweicheng <yangweicheng@jlishop.com>
2018-10-22 15:16:08 +0800

    第二次提交

---
+++ src/com/base/chapter/Bird.java
@@ -0,0 +1,55 @@
+package com.base.chapter;
+
+public class Bird {
+	private String id;
+	private String category;
+	private String noise;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getNoise() {
+		return noise;
+	}
+	public void setNoise(String noise) {
+		this.noise = noise;
+	}
+	
+	public Bird() {
+		super();
+		this.id = "1";
+		this.category = "鸟";
+		this.noise = "小燕子等等我";
+	}
+	public Bird(String id, String category, String noise) {
+		super();
+		this.id = id;
+		this.category = category;
+		this.noise = noise;
+	}
+
+	public void showNoise(Bird bird){
+		System.out.println(bird.getNoise());
+	}
+	
+	public void showCategoty(Bird bird){
+		System.out.println(bird.getCategory());
+	}
+	
+	public static void main(String[] args) {
+		Bird bird = new Bird();
+		System.out.println(bird.getId());
+		System.out.println(bird.id);
+
+	}
+}
