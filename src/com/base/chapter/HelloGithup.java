6d1f1ad42e1d8b898085460d0665d81160a92a0b
yangweicheng <yangweicheng@jlishop.com>
2018-10-30 12:47:15 +0800

    commit business one

--- src/com/base/chapter/HelloGithup.java
+++ src/com/base/chapter/HelloGithup.java
@@ -3,6 +3,7 @@ package com.base.chapter;
 public class HelloGithup {
 	public static void main(String[] args) {
 		System.out.println("hello github!");
+		System.out.println("hello this is my business one!");
 		System.out.println("hello this is my private One!");
 	}
 }
