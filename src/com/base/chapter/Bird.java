8b76f9f909b93ae7dcebc9651eef12989fb84d14
weiChengPrivateHome1 <18721015817@163.com>
2018-10-30 11:19:47 +0800

    private one commit

--- src/com/base/chapter/Bird.java
+++ src/com/base/chapter/Bird.java
@@ -1,10 +1,18 @@
 package com.base.chapter;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Bird {
 	private String id;
 	private String category;
 	private String noise;
-	
+
+	public static final HashMap<String,String> map = new HashMap<String,String>();
+	static{
+		map.put("1","1");
+	}
 	
 	public String getId() {
 		return id;
@@ -50,6 +58,23 @@ public class Bird {
 		Bird bird = new Bird();
 		System.out.println(bird.getId());
 		System.out.println(bird.id);
+
+		String s = "111";
+		String s2 = "111";
+
+		String s3 = new String("111");
+		System.out.println(s == s2);
+		System.out.println(s.substring(0,1) == s2.substring(0,1));
+
+		System.out.println(s == s3);
+
+		String s4 = "111111".substring(0,3);
+		System.out.println("s4:"+s4);
+		System.out.println(s == s4);
+		System.out.println(s == s4.intern());
+
+		System.out.println(map.get("1"));
+
 
 	}
 }
--- src/com/base/chapter/HelloGithup.java
+++ src/com/base/chapter/HelloGithup.java
@@ -3,5 +3,6 @@ package com.base.chapter;
 public class HelloGithup {
 	public static void main(String[] args) {
 		System.out.println("hello github!");
+		System.out.println("hello this is my private One!");
 	}
 }
